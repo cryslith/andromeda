8899772544527d99035111eca277980a6c6417ea
Lily Chung <lkdc@mit.edu>
2017-07-24 22:29:06 -0400

    urgent notifications

--- andromeda.py
+++ andromeda.py
@@ -18,34 +18,30 @@ class Andromeda(object):
         self.realm = options.get('realm', 'ATHENA.MIT.EDU')
         self.pushover_token = options['pushover_token']
         self.pushover_user = options['pushover_user']
+        self.priority = options.get('priority')
 
         self.last_time = time.time()
 
-        self.zp = zpipe.ZPipe(["zpipe"], self.check_zgram)
+        self.zp = zpipe.ZPipe(["zpipe"], self.handle)
         self.zp.subscribe(self.room)
         if self.largeroom:
             self.zp.subscribe(self.largeroom)
 
     def info(self, cls, instance, message):
+        time.sleep(1)
         nn = zpipe.Zephyrgram(self.name, cls, instance, None, 'auto', False,
                               [self.zsig, message])
         self.zp.zwrite(nn)
 
-    def check_zgram(self, _, zgram):
-        cls = zgram.cls.lower()
-        instance = zgram.instance.lower()
-        try:
-            sender, foundrealm = zgram.sender.split("@")
-            _, message = zgram.fields
-        except ValueError:
-            return
+    def reject_info(self, cls, instance, again):
+        self.info(cls, instance,
+                  '{} could not be notified; {}'.format(
+                self.user,
+                'try again later' if again else 'do not try again'))
 
-        if foundrealm != self.realm or not zgram.auth:
-            return
-        if 'auto' in zgram.opcode.lower():
-            return
-
-        self.handle(cls, instance, sender, message)
+    def success_info(self, cls, instance, note_type):
+        self.info(cls, instance,
+                  '{} sent to {}'.format(note_type, self.user))
 
     def check_rate(self):
         if self.last_time + 5 <= time.time():
@@ -54,36 +50,62 @@ class Andromeda(object):
         else:
             return False
 
-    def handle(self, cls, instance, sender, message):
-        if ((cls == self.room and instance == self.name) or
-            cls == self.largeroom):
-            if sender in self.blocklist:
-                self.info(cls, instance,
-                     '{}: you are blocked from sending notifications '
-                     'to {}'. format(sender, self.user))
-                return
-            if not self.check_rate():
-                self.info(cls, instance,
-                     "{} could not be notified; try again later".format(self.user))
-                return
+    def handle(self, _, zgram):
+        cls = zgram.cls.lower()
+        instance = zgram.instance.lower()
+        opcode = zgram.opcode.lower()
 
-            if cls == self.largeroom and sender == self.user:
-                send = '{}: {}'.format(instance, message)
-            else:
-                send = '{}-{}: {}'.format(sender, instance, message)
-            resp = requests.post("https://api.pushover.net/1/messages.json",
-                                 data={"token": self.pushover_token,
-                                       "user": self.pushover_user,
-                                       "message": send})
-            if resp.status_code == 200:
-                self.info(cls, instance, "notification sent to {}".format(self.user))
-            elif resp.status_code >= 400 and resp.status_code <= 499:
-                self.info( cls, instance,
-                     "{} could not be notified; do not try again".format(self.user))
-            else:
-                self.info(cls, instance,
-                     "{} could not be notified; try again later".format(self.user))
+        if not ((cls == self.room and instance == self.name) or
+                cls == self.largeroom):
             return
+
+        try:
+            sender, foundrealm = zgram.sender.split("@")
+            _, message = zgram.fields
+        except ValueError:
+            return
+
+        if foundrealm != self.realm or not zgram.auth:
+            return
+        if 'auto' in opcode:
+            return
+
+        if sender in self.blocklist:
+            self.info(cls, instance,
+                      '{}: you are blocked from sending notifications '
+                      'to {}'.format(sender, self.user))
+            return
+
+        priority = 1 if 'urgent' in opcode else 0
+        note_type = {0: 'notification',
+                     1: 'urgent notification'}[priority]
+        if priority > 0 and not self.priority:
+            self.info(cls, instance,
+                      '{} has disabled {}s'.format(self.user, note_type))
+            return
+
+        if not self.check_rate():
+            self.reject_info(cls, instance, True)
+            return
+
+        if cls == self.largeroom and sender == self.user:
+            notification = '{}: {}'.format(instance, message)
+        else:
+            notification = '{}-{}: {}'.format(sender, instance, message)
+
+        resp = requests.post(
+            'https://api.pushover.net/1/messages.json',
+            data={'token': self.pushover_token,
+                  'user': self.pushover_user,
+                  'message': notification,
+                  'priority': priority})
+
+        if resp.status_code == 200:
+            self.success_info(cls, instance, note_type)
+        elif resp.status_code >= 400 and resp.status_code <= 499:
+            self.reject_info(cls, instance, False)
+        else:
+            self.reject_info(cls, instance, True)
 
 
 def main():
